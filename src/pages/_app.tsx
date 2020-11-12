5325f57a77977bae588dc5b414ae013fb7209828
coderitual <skowronkow@gmail.com>
2020-11-12 11:56:53 +0100

    Add tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import '../styles/index.css';
 
 const GlobalStyle = createGlobalStyle`
   html,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Layout from '../components/Layout/Layout';
+import Navigation from '../components/Navigation';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <title>Cats</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navigation />
       <Layout>
         <h1>Hello fellows</h1>
         <p> Hello cats</p>
