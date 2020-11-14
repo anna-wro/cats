a22d2b4da9f4bca874a1c284ea234fbf5a04c388
anna-wro <wro.online@gmail.com>
2020-11-14 18:23:28 +0100

    Display basic plant list

---
+++ src/components/Plants/PlantList.tsx
@@ -0,0 +1,26 @@
+type PlantType = {
+  slug: string;
+  name: {
+    pl: string;
+    lat: string;
+  };
+  isSafe: boolean;
+  img: string;
+};
+
+type PlantGroupType = {
+  [key: string]: PlantType;
+};
+
+export default function PlantList({ plants }: PlantGroupType) {
+  // FIXME: Property 'map' does not exist on type 'PlantType'
+  return (
+    <>
+      {plants.map((plant) => (
+        <div>
+          {plant.name.pl} - {plant.isSafe ? 'is safe' : 'is toxic'}
+        </div>
+      ))}
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 import Layout from '../components/Layout/Layout';
 import Navigation from '../components/Navigation';
+import PlantList from '../components/Plants/PlantList';
+import fs from 'fs';
+import path from 'path';
 
-export default function Home() {
+export default function Home({ plants }) {
   return (
     <div>
       <Head>
@@ -13,7 +16,22 @@ export default function Home() {
       <Layout>
         <h1>Hello fellows</h1>
         <p> Hello cats</p>
+        <PlantList plants={plants} />
       </Layout>
     </div>
   );
+}
+
+const DATA_PATH = path.join(process.cwd(), 'src/data/plants');
+const dataFilePaths = fs
+  .readdirSync(DATA_PATH)
+  .filter((path) => /\.json?$/.test(path));
+
+export function getStaticProps() {
+  const plants = dataFilePaths.map((filePath) => {
+    const source = fs.readFileSync(path.join(DATA_PATH, filePath));
+    const obj = JSON.parse(source.toString());
+    return obj;
+  });
+  return { props: { plants } };
 }
