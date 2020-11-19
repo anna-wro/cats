b6c24d5abc623c6cf3cc2e5a037d9cfc079927d9
anna-wro <wro.online@gmail.com>
2020-11-19 22:25:59 +0100

    Utils / sort plant by name

--- src/components/Plants/PlantsFacade.tsx
+++ src/components/Plants/PlantsFacade.tsx
@@ -1,11 +1,12 @@
 import PlantList from './PlantList';
 import safe from '../../data/plants/safe.json';
 import toxic from '../../data/plants/toxic.json';
-import { shuffleArray } from '../../utils/shuffleArray';
+import { sortByName } from '../../utils/array';
 
 export default function PlantsFacade() {
   const plants = [...safe, ...toxic];
-  const shuffledPlants = shuffleArray(plants);
 
-  return <PlantList plants={shuffledPlants} />;
+  const sortedPlants = plants.sort(sortByName('pl'));
+
+  return <PlantList plants={sortedPlants} />;
 }
