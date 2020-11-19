904c009aafe66b21ce015187b431ac9b5078e591
anna-wro <wro.online@gmail.com>
2020-11-19 22:40:18 +0100

    display both random and sorted

--- src/components/Plants/PlantsFacade.tsx
+++ src/components/Plants/PlantsFacade.tsx
@@ -1,12 +1,27 @@
 import PlantList from './PlantList';
 import safe from '../../data/plants/safe.json';
 import toxic from '../../data/plants/toxic.json';
-import { sortByName } from '../../utils/array';
+import { shuffleArray, sortByName } from '../../utils/array';
 
 export default function PlantsFacade() {
   const plants = [...safe, ...toxic];
-
   const sortedPlants = plants.sort(sortByName('pl'));
+  const shuffledPlants = shuffleArray(plants);
 
-  return <PlantList plants={sortedPlants} />;
+  return (
+    <div className="grid grid-flow-col auto-cols-auto">
+      <div>
+        <div>
+          <div className="text-lg py-6">From 🅰 to 🆉:</div>
+          <PlantList plants={sortedPlants} />
+        </div>
+      </div>
+      <div>
+        <div>
+          <div className="text-lg py-6">Random 🎲:</div>
+          <PlantList plants={shuffledPlants} />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,5 +1,5 @@
 export function shuffleArray(array: Array<Object>) {
-  const shuffledArray = array;
+  const shuffledArray = [...array];
 
   for (let i = shuffledArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
