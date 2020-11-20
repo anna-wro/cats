f814b5b243a707fad6eb313b25e4e8ad18201419
anna-wro <wro.online@gmail.com>
2020-11-20 19:23:31 +0100

    Display only one list

--- src/components/Plants/PlantsFacade.tsx
+++ src/components/Plants/PlantsFacade.tsx
@@ -1,26 +1,18 @@
 import PlantList from './PlantList';
 import safe from '../../data/plants/safe.json';
 import toxic from '../../data/plants/toxic.json';
-import { shuffleArray, sortByName } from '../../utils/array';
+import {sortByName } from '../../utils/array';
 
 export default function PlantsFacade() {
   const plants = [...safe, ...toxic];
   const sortedPlants = plants.sort(sortByName('pl'));
-  const shuffledPlants = shuffleArray(plants);
 
   return (
     <div className="grid grid-flow-col auto-cols-auto">
       <div>
-        <div>
-          <div className="text-lg py-6">From 🅰 to 🆉:</div>
-          <PlantList plants={sortedPlants} />
-        </div>
-      </div>
-      <div>
-        <div>
-          <div className="text-lg py-6">Random 🎲:</div>
-          <PlantList plants={shuffledPlants} />
-        </div>
+        <div className="text-lg py-6">From 🅰 to 🆉:</div>
+        <div className="text pb-6  text-gray-600">{plants.length} plants</div>
+        <PlantList plants={sortedPlants} />
       </div>
     </div>
   );
