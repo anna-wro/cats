d399ea45ecc1dc4f02f6100312ce63c893fc9d33
Anna Wróbel <anna-wro@users.noreply.github.com>
2020-11-20 14:27:33 +0100

    Utils for sorting and random order of plants (#4)

---
+++ src/utils/array.ts
@@ -0,0 +1,22 @@
+export function shuffleArray(array: Array<Object>) {
+  const shuffledArray = [...array];
+
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+
+  return shuffledArray;
+}
+
+export function sortByName(lang: string) {
+  let sortOrder = 1;
+  if (lang[0] === '-') {
+    sortOrder = -1;
+    lang = lang.substr(1);
+  }
+  return function (a, b) {
+    const result = a['name'][lang] < b['name'][lang] ? -1 : a['name'][lang] > b['name'][lang] ? 1 : 0;
+    return result * sortOrder;
+  };
+}
