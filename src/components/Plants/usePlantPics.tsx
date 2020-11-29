e25adf531a94e1115c795f5105953fceda1352ca
anna-wro <wro.online@gmail.com>
2020-11-29 14:31:27 +0100

    usePlantPics - return type

--- src/components/Plants/usePlantPics.tsx
+++ src/components/Plants/usePlantPics.tsx
@@ -2,6 +2,18 @@ import { useEffect, useState } from 'react';
 import { setMultipleParams } from '../../utils/url';
 import axios from 'axios';
 
+export type PlantPhotosType = {
+  farm: number;
+  id: string;
+  isfamily: boolean;
+  isfriend: boolean;
+  ispublic: boolean;
+  owner: string;
+  secret: string;
+  server: string;
+  title: string;
+};
+
 const API_URL = 'https://www.flickr.com/services/rest/';
 const API_KEY = process.env.NEXT_PUBLIC_FLICKR_API_KEY;
 
@@ -16,8 +28,7 @@ const settings = {
   per_page: 12,
 };
 
-// TODO add return type
-export default function usePlantPics(name: string) {
+export default function usePlantPics(name: string): PlantPhotosType[] {
   let [photos, setPhotos] = useState([]);
   const url = setMultipleParams(settings, API_URL);
 
