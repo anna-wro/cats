b98dc41630b61c7ffeff8429c55ed3c6e09ec5f0
anna-wro <wro.online@gmail.com>
2020-11-29 14:21:27 +0100

    set multiple query params

--- src/components/Plants/usePlantPics.tsx
+++ src/components/Plants/usePlantPics.tsx
@@ -1,23 +1,31 @@
 import { useEffect, useState } from 'react';
+import { setMultipleParams } from '../../utils/url';
 import axios from 'axios';
 
-const API_URL =
-  'https://www.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1';
+const API_URL = 'https://www.flickr.com/services/rest/';
 const API_KEY = process.env.NEXT_PUBLIC_FLICKR_API_KEY;
+
+const settings = {
+  method: 'flickr.photos.search',
+  format: 'json',
+  nojsoncallback: 1,
+  api_key: API_KEY,
+  sort: 'relevance',
+  license: '1,2,3,4,5,9,10',
+  content_type: '1',
+  per_page: 12,
+};
 
 // TODO add return type
 export default function usePlantPics(name: string) {
   let [photos, setPhotos] = useState([]);
+  const url = setMultipleParams(settings, API_URL);
 
   // TODO: use fetch instead?
   useEffect(() => {
-    axios
-      .get(
-        `${API_URL}&api_key=${API_KEY}&sort=relevance&text=${name}&license=1,2,3,4,5,9,10&content_type=1&per_page=12`,
-      )
-      .then((response) => {
-        setPhotos(response.data.photos.photo);
-      });
+    axios.get(`${url}&text=${name}`).then((response) => {
+      setPhotos(response.data.photos.photo);
+    });
   }, []);
 
   return photos;
