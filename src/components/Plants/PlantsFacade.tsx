2acf7a9de08352c87c58575b6030f349bc6d81e1
coderitual <skowronkow@gmail.com>
2020-12-17 13:19:36 +0100

    Add throttling

--- src/components/Plants/PlantsFacade.tsx
+++ src/components/Plants/PlantsFacade.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { matchSorter } from 'match-sorter';
 import { polishPlurals } from 'polish-plurals';
 import PlantList from './PlantList';
@@ -6,13 +6,40 @@ import Search from '../Search';
 import safe from 'data/plants/safe.json';
 import toxic from 'data/plants/toxic.json';
 import { sortByName } from 'utils/array';
+import { useThrottle } from 'use-throttle';
+
+function usePlantSearch(searchTerm, plants) {
+  const [results, setResults] = useState(plants);
+  const throttled = useThrottle(searchTerm, 500);
+
+  useEffect(() => {
+    if (throttled) {
+      if (throttled.trim() !== '') {
+        let current = true;
+
+        const queryResults = matchSorter(plants, searchTerm, {
+          threshold: matchSorter.rankings.WORD_STARTS_WITH,
+          keys: ['name.pl', 'name.en', 'name.lat'],
+        });
+
+        setResults(queryResults);
+        return () => (current = false);
+      }
+    } else if (searchTerm === '') {
+      setResults(plants);
+    }
+  }, [throttled]);
+
+  return results;
+}
 
 export default function PlantsFacade() {
   const plants = [...safe, ...toxic];
   const sortedPlants = plants.sort(sortByName('pl'));
 
   const [query, setQuery] = useState('');
-  const [results, setResults] = useState(sortedPlants);
+
+  const results = usePlantSearch(query, plants);
   const counter = results.length;
   const plantPluralForm = polishPlurals(
     'roślinę',
@@ -22,13 +49,7 @@ export default function PlantsFacade() {
   );
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const queryResults = matchSorter(plants, e.currentTarget.value, {
-      threshold: matchSorter.rankings.WORD_STARTS_WITH,
-      keys: ['name.pl', 'name.en', 'name.lat'],
-    });
-
     setQuery(e.currentTarget.value);
-    setResults(queryResults);
   }
 
   // FIXME: typescript
