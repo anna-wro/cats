f414835dc61d9240abd797b6757cd04b25946046
anna-wro <wro.online@gmail.com>
2020-12-19 14:49:48 +0100

    add very frowned svg

--- src/components/PlantFiche/SafetyBadge.tsx
+++ src/components/PlantFiche/SafetyBadge.tsx
@@ -6,11 +6,21 @@ export default function SafetyBadge({ danger }: SafetyBadgeProps) {
   return (
     <div
       className={` ${
-        danger === 0 ? 'bg-blue-light' : 'bg-orange-light'
+        danger === 0
+          ? 'bg-blue-light'
+          : danger === 3
+          ? 'bg-red-light'
+          : 'bg-orange-light'
       } w-12 h-12 p-2.5 rounded-2xl`}
     >
       <img
-        src={danger === 0 ? '/smile.svg' : '/frown.svg'}
+        src={
+          danger === 0
+            ? '/smile.svg'
+            : danger === 3
+            ? '/very-frowned.svg'
+            : '/frown.svg'
+        }
         width={30}
         height={30}
       />
