f60fca041e2ca2192efd6dce974539090047deed
anna-wro <wro.online@gmail.com>
2020-12-23 19:24:51 +0100

    round badge

--- src/components/SafetyBadge/SafetyBadge.tsx
+++ src/components/SafetyBadge/SafetyBadge.tsx
@@ -10,8 +10,10 @@ export default function SafetyBadge({ danger, withBorder }: SafetyBadgeProps) {
 
   return (
     <div
-      className={`w-12 h-12 rounded-2xl flex items-center justify-center 
-      ${scheme.bg} ${withBorder && 'border-4 border-white'}`}
+      className={`w-12 h-12 flex items-center justify-center 
+      ${scheme.bg} ${
+        withBorder ? 'border-4 border-white rounded-full' : 'rounded-2xl'
+      }`}
     >
       <img src={scheme.icon} width={28} height={28} />
     </div>
