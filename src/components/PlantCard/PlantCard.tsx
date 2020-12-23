37f7152b706cea989e925af5a54c37878397b4da
anna-wro <wro.online@gmail.com>
2020-12-23 16:28:51 +0100

    Create basic breakpoint

---
+++ src/components/PlantCard/PlantCard.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link';
+import useThumbnail from 'components/PlantFiche/useThumbnail';
+import { getPhotoUrl } from 'utils/flickr';
+import SafetyBadge from 'components/PlantFiche/SafetyBadge';
+import ImageContainer from 'components/Image/ImageContainer';
+
+export type PlantType = Readonly<{
+  name: {
+    pl: string[];
+    en: string[];
+    lat: string;
+  };
+  slug: string;
+  thumbnailID: string;
+  queryImage?: string;
+  danger: number;
+  symptoms?: string;
+  note?: string;
+  source?: string[];
+}>;
+
+type PlantFicheProps = Readonly<{
+  plant: PlantType;
+}>;
+
+export default function PlantCard({ plant }: PlantFicheProps) {
+  const thumbnail = useThumbnail(plant.thumbnailID);
+  const url = thumbnail ? getPhotoUrl(thumbnail) : null;
+
+  return (
+    <Link href={plant.slug}>
+      <div className="flex h-32 w-100 max-w-xs shadow rounded-lg cursor-pointer">
+        <div className="w-1/2 rounded-l-lg overflow-hidden bg-gray-light bg-opacity-30">
+          {url && (
+            <ImageContainer
+              src={url.bigger}
+              thumbnail={url.thumbnail}
+              height={128}
+              width={150}
+              alt={plant.name.lat}
+            />
+          )}
+        </div>
+        <div className="w-1/2 rounded-r-lg p-5">
+          <div className="flex flex-col items-center justify-center">
+            <SafetyBadge danger={plant.danger} />
+            <div className="text-dark text-sm text-center leading-4 pt-2">
+              {plant.name.pl[0]}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/components/Plants/PlantList.tsx
+++ src/components/Plants/PlantList.tsx
@@ -1,13 +1,26 @@
 import PlantFiche from 'components/PlantFiche/PlantFiche';
 import type { PlantType } from 'components/PlantFiche/PlantFiche';
+import useWindowSize from 'utils/useWindowSize';
 
 type PlantGroupType = {
   plants: PlantType[];
 };
 
+const MOBILE_WIDTH = 480;
+
 export default function PlantList({ plants }: PlantGroupType) {
-  return (
-    <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-y-10 mt-20 gap-x-28">
+  const { width } = useWindowSize();
+
+  return width <= MOBILE_WIDTH ? (
+    <div className="mt-10">
+      {plants.map((plant) => (
+        <div className="mb-6">
+          <PlantFiche key={plant.slug} plant={plant} />
+        </div>
+      ))}
+    </div>
+  ) : (
+    <div className="grid lg:grid-cols- md:grid-cols-2 gap-y-10 mt-20 gap-x-28">
       {plants.map((plant) => (
         <PlantFiche key={plant.slug} plant={plant} />
       ))}
