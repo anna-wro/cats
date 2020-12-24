227355d7e2d9f72091907a98b68dddf9b12a0a6a
anna-wro <wro.online@gmail.com>
2020-12-24 23:04:32 +0100

    display only when danger

--- src/components/PlantDetails/PlantSpecs.tsx
+++ src/components/PlantDetails/PlantSpecs.tsx
@@ -13,17 +13,20 @@ export default function PlantSpecs({ plant }: PlantDetailsType) {
       <div className="text-dark text-2xl font-semibold mb-6">
         {makeStartCase(plant.name.pl[0])}
       </div>
-      <div className="bg-red bg-opacity-20 rounded-2xl p-4 flex items-center mb-8">
-        <div className="bg-white flex items-center justify-center rounded-full h-12 w-12 mr-4 flex-shrink-0">
-          <img className="w-10 h-10" src="/pulse.svg" />
-        </div>
-        <div>
-          <div className="text-sm font-light">Twój kot to zjadł?</div>
-          <div className="text-sm font-light">
-            Dowiedz się, <span className="font-bold underline">co zrobić.</span>
+      {plant.danger !== 0 && (
+        <div className="bg-red bg-opacity-20 rounded-2xl p-4 flex items-center mb-8">
+          <div className="bg-white flex items-center justify-center rounded-full h-12 w-12 mr-4 flex-shrink-0">
+            <img className="w-10 h-10" src="/pulse.svg" />
+          </div>
+          <div>
+            <div className="text-sm font-light">Twój kot to zjadł?</div>
+            <div className="text-sm font-light">
+              Dowiedz się,{' '}
+              <span className="font-bold underline">co zrobić.</span>
+            </div>
           </div>
         </div>
-      </div>
+      )}
       <div className="mb-6">
         <div className="text-lg font-semibold mb-2">Zagrożenie</div>
         <div className="text-xs">{plant.danger}</div>
