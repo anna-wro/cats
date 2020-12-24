1d6ea84089299ae526f54cd63fb8033d753aab3c
anna-wro <wro.online@gmail.com>
2020-12-24 14:00:26 +0100

    add search icon

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -5,13 +5,15 @@ type SearchPropsType = {
 
 export default function Search({ query, onChange }: SearchPropsType) {
   return (
-    <input
-      type="search"
-      placeholder="Fiołek"
-      value={query}
-      className="text-lg block mt-20 h-16 w-full p-6 shadow-lg rounded-lg border border-transparent 
+    <div className="search relative">
+      <input
+        type="search"
+        placeholder="Fiołek"
+        value={query}
+        className="text-lg block mt-20 h-16 w-full p-6 pl-14 shadow-lg rounded-lg border border-transparent 
         focus:ring-4 focus:ring-blue focus:ring-opacity-30 focus:border-blue focus:outline-none"
-      onChange={(e) => onChange(e)}
-    />
+        onChange={(e) => onChange(e)}
+      />
+    </div>
   );
 }
