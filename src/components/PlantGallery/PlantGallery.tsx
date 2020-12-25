98ca4a23f244fe2e09c3d03c7c9e8dcee3eac897
anna-wro <wro.online@gmail.com>
2020-12-25 15:28:01 +0100

    adjust close button position

--- src/components/PlantGallery/PlantGallery.tsx
+++ src/components/PlantGallery/PlantGallery.tsx
@@ -4,8 +4,8 @@ type PlantGalleryType = Readonly<{ plant: PlantType }>;
 
 export default function PlantGallery({ plant }: PlantGalleryType) {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
-      plant gallery plabeholder
+    <div className="flex flex-col items-center justify-center w-full h-full bg-blue-light">
+      plant gallery placeholder
     </div>
   );
 }
