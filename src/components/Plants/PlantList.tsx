2c3a098fbd41c5a3850cd175a68bce32400cfc4a
anna-wro <wro.online@gmail.com>
2021-01-11 20:14:25 +0100

    Remove logo on mobile

--- src/components/Plants/PlantList.tsx
+++ src/components/Plants/PlantList.tsx
@@ -1,19 +1,17 @@
 import PlantFiche from 'components/PlantFiche/PlantFiche';
 import PlantCard from 'components/PlantCard/PlantCard';
 import type { PlantType } from 'components/PlantFiche/PlantFiche';
-import useWindowSize from 'utils/useWindowSize';
+import {useWindowSize} from 'utils/useWindowSize';
 
 type PlantListType = Readonly<{
   plants: PlantType[];
   query: string;
 }>;
 
-const MOBILE_WIDTH = 480;
-
 export default function PlantList({ plants, query }: PlantListType) {
-  const { width } = useWindowSize();
+  const { isMobile } = useWindowSize();
 
-  return width <= MOBILE_WIDTH ? (
+  return isMobile ? (
     <>
       {plants.map((plant) => (
         <div className="mb-6">
@@ -22,7 +20,7 @@ export default function PlantList({ plants, query }: PlantListType) {
       ))}
     </>
   ) : (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5   gap-6">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
       {plants.map((plant) => (
         <PlantCard key={plant.slug} plant={plant} query={query} />
       ))}
