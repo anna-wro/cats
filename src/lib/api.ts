3d485fbe0ebfb3e070c4f8ad9fac28412696e1ac
coderitual <skowronkow@gmail.com>
2021-01-28 13:06:19 +0100

    Fix api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -17,50 +17,51 @@ function mapRecordToPlant(record) {
   };
 }
 
+async function getPlantsByType(type) {
+  return new Promise((resolve, reject) => {
+    const base = Airtable.base('app0awhBu3GphBQkq');
+    const res = [];
+    base(type)
+      .select({
+        view: 'Grid view',
+        // BUG: Airtable throws error when max records value is equal or greater than 100
+        // https://community.airtable.com/t/cannot-read-property-offset-of-undefined/34847
+        maxRecords: 1000,
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          records.forEach(function (record) {
+            if (record.get('ID')) {
+              const plant = mapRecordToPlant(record.fields);
+              res.push(plant);
+            }
+          });
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            reject(res);
+            return;
+          }
+          resolve(res);
+        },
+      );
+  });
+}
+
 export async function getAllPlants() {
-  const base = Airtable.base('app0awhBu3GphBQkq');
-
   try {
-    const result: {
-      safe: Array<Object>;
-      toxic: Array<Object>;
-    } = await new Promise((resolve, reject) => {
-      const res = {
-        safe: [],
-        toxic: [],
-      };
+    const [Toxic, Safe] = await Promise.all([
+      getPlantsByType('Safe'),
+      getPlantsByType('Toxic'),
+    ]);
 
-      ['Safe', 'Toxic'].forEach((type) => {
-        base(type)
-          .select({
-            view: 'Grid view',
-            // BUG: Airtable throws error when max records value is equal or greater than 100
-            // https://community.airtable.com/t/cannot-read-property-offset-of-undefined/34847
-            maxRecords: 1000,
-          })
-          .eachPage(
-            function page(records, fetchNextPage) {
-              records.forEach(function (record) {
-                if (record.get('ID')) {
-                  const plant = mapRecordToPlant(record.fields);
-                  res[type.toLowerCase()].push(plant);
-                }
-              });
-              fetchNextPage();
-            },
-            function done(err) {
-              if (err) {
-                console.error(err);
-                reject(res);
-                return;
-              }
-              resolve(res);
-            },
-          );
-      });
-    });
-    return result;
+    return {
+      toxic: Toxic,
+      safe: Safe,
+    };
   } catch (e) {
-    console.error('Error', e);
+    console.error('API Error getAllPlants', e);
   }
 }
