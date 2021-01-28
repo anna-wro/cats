be99341f3ae1667530a91929d4510da11ad373fa
coderitual <skowronkow@gmail.com>
2021-01-28 12:52:22 +0100

    Fix api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,15 +3,15 @@ import Airtable from 'airtable';
 function mapRecordToPlant(record) {
   return {
     name: {
-      pl: record['name.pl'].split(',').map((value) => value.trim()),
-      en: record['name.en'].split(',').map((value) => value.trim()),
-      lat: record['name.lat'],
+      pl: record['name.pl']?.split(',').map((value) => value.trim()),
+      en: record['name.en']?.split(',').map((value) => value.trim()),
+      lat: record['name.lat'] ?? null,
     },
-    slug: record.slug,
-    imageID: record.imageID.split(',').map((value) => value.trim()),
-    danger: record.danger,
-    source: record.source.split(',').map((value) => value.trim()),
-    ID: record.ID,
+    slug: record.slug ?? null,
+    imageID: record.imageID?.split(',').map((value) => value.trim()),
+    danger: Number(record.danger ?? -1),
+    source: record.source?.split(',').map((value) => value.trim()),
+    ID: record.ID ?? null,
     symptoms: record.symptoms ?? null,
     note: record.note ?? null,
   };
@@ -20,42 +20,47 @@ function mapRecordToPlant(record) {
 export async function getAllPlants() {
   const base = Airtable.base('app0awhBu3GphBQkq');
 
-  const result: {
-    safe: Array<Object>;
-    toxic: Array<Object>;
-  } = await new Promise((resolve, reject) => {
-    const res = {
-      safe: [],
-      toxic: [],
-    };
+  try {
+    const result: {
+      safe: Array<Object>;
+      toxic: Array<Object>;
+    } = await new Promise((resolve, reject) => {
+      const res = {
+        safe: [],
+        toxic: [],
+      };
 
-    ['Safe', 'Toxic'].forEach((type) => {
-      base(type)
-        .select({
-          view: 'Grid view',
-          // BUG: Airtable throws error when max records value is equal or greater than 100
-          // https://community.airtable.com/t/cannot-read-property-offset-of-undefined/34847
-          maxRecords: 1000,
-        })
-        .eachPage(
-          function page(records, fetchNextPage) {
-            records.forEach(function (record) {
-              const plant = mapRecordToPlant(record.fields);
-              res[type.toLowerCase()].push(plant);
-            });
-            fetchNextPage();
-          },
-          function done(err) {
-            if (err) {
-              console.error(err);
-              reject(res);
-              return;
-            }
-            resolve(res);
-          },
-        );
+      ['Safe', 'Toxic'].forEach((type) => {
+        base(type)
+          .select({
+            view: 'Grid view',
+            // BUG: Airtable throws error when max records value is equal or greater than 100
+            // https://community.airtable.com/t/cannot-read-property-offset-of-undefined/34847
+            maxRecords: 1000,
+          })
+          .eachPage(
+            function page(records, fetchNextPage) {
+              records.forEach(function (record) {
+                if (record.get('ID')) {
+                  const plant = mapRecordToPlant(record.fields);
+                  res[type.toLowerCase()].push(plant);
+                }
+              });
+              fetchNextPage();
+            },
+            function done(err) {
+              if (err) {
+                console.error(err);
+                reject(res);
+                return;
+              }
+              resolve(res);
+            },
+          );
+      });
     });
-  });
-
-  return result;
+    return result;
+  } catch (e) {
+    console.error('Error', e);
+  }
 }
