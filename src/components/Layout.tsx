f1cba24d9e3849f19468a750078fc7883eb8ac3a
anna-wro <wro.online@gmail.com>
2021-06-19 11:57:58 +0200

    fix no-page-custom-font

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,13 @@
 import Head from 'next/head';
+import React from 'react';
 import Footer from './Footer';
 
 export default function Layout({
   children,
   title = 'This is the default title',
+}: {
+  children: React.ReactNode;
+  title: string;
 }) {
   return (
     <div className="flex flex-col min-h-screen font-main text-dark">
@@ -16,10 +20,6 @@ export default function Layout({
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Montserrat:wght@300;400;500;600;700&family=Nunito:wght@900&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       {children}
       <Footer />
