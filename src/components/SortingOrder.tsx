c99d00bfcf3de94af9234bac8f10993f077b4a1c
anna-wro <wro.online@gmail.com>
2021-06-19 18:02:29 +0200

    🐞 fix sorting by name

--- src/components/SortingOrder.tsx
+++ src/components/SortingOrder.tsx
@@ -9,7 +9,7 @@ export default function SortingOrder({ value, onChange }: SortingOrderType) {
   return (
     <div className="w-40">
       <select
-        value={value}
+        defaultValue={value}
         onBlur={e => onChange(e)}
         className="sorting-order appearance-none border border-gray rounded-lg py-px pl-2 pr-5 
       text-xs text-dark font-bold focus:border-blue focus:outline-none"
