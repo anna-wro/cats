1bc606192914d2a6e7f9ca3d12dd7d7b12f21d08
anna-wro <wro.online@gmail.com>
2021-06-26 20:56:44 +0200

    make it work

--- src/components/PlantGallery/PlantGalleryItem.tsx
+++ src/components/PlantGallery/PlantGalleryItem.tsx
@@ -6,26 +6,22 @@ import ScrollBox from 'components/PlantGallery/ScrollBox';
 export default function PlantGalleryItem({ imgAlt, ID, scrollRef }) {
   const photo = usePhoto(ID);
 
-  return (
+  return photo ? (
     <ScrollBox scrollRef={scrollRef}>
       <div className="h-full my-14">
-        {photo && (
-          <>
-            <ImageContainer
-              src={photo.links.xl}
-              fallback={photo.links.l}
-              thumbnail={photo.links.xs}
-              alt={imgAlt}
-              rounded
-            />
-            <Credits
-              source={photo.links.source}
-              owner={photo.owner}
-              license={photo.license}
-            />
-          </>
-        )}
+        <ImageContainer
+          src={photo.links.xl}
+          fallback={photo.links.l}
+          thumbnail={photo.links.xs}
+          alt={imgAlt}
+          rounded
+        />
+        <Credits
+          source={photo.links.source}
+          owner={photo.owner}
+          license={photo.license}
+        />
       </div>
     </ScrollBox>
-  );
+  ) : null;
 }
