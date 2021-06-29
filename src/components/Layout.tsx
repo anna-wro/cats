f6ef416bf21087a4ba9a8c3ee922d902c7c152f6
anna-wro <wro.online@gmail.com>
2021-06-29 19:50:03 +0200

    🔍 verify site in google

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,6 +19,10 @@ export default function Layout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=5.0, viewport-fit=cover, shrink-to-fit=no"
         />
+        <meta
+          name="google-site-verification"
+          content="zvDdSyW4U7kvunGhvL_U2pq0tjfLcz8m9QW079aSubk"
+        />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
       </Head>
