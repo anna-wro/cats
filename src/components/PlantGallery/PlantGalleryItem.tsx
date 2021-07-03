daa8b6a850a6852dac43c98a0021b3ea03bf8931
Anna Wróbel <anna-wro@users.noreply.github.com>
2021-07-03 17:05:38 +0200

    🔧 refactor gallery to remove eslint error (#75)

---
+++ src/components/PlantGallery/PlantGalleryItem.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from 'react';
+import Credits from './Credits';
+import usePhoto from 'utils/usePhoto';
+import ImageContainer from 'components/Image/ImageContainer';
+import ScrollBox from 'components/PlantGallery/ScrollBox';
+
+export default function PlantGalleryItem({
+  imgAlt,
+  ID,
+  scrollRef,
+  onDataFetched,
+  scrollBoxEnabled,
+}) {
+  const photo = usePhoto(ID);
+
+  useEffect(() => {
+    if (photo) {
+      onDataFetched();
+    }
+  }, [photo, onDataFetched]);
+
+  return scrollBoxEnabled && photo ? (
+    <ScrollBox scrollRef={scrollRef}>
+      <div className="h-full my-14">
+        <ImageContainer
+          src={photo.links.xl}
+          fallback={photo.links.l}
+          thumbnail={photo.links.xs}
+          alt={imgAlt}
+          rounded
+        />
+        <Credits
+          source={photo.links.source}
+          owner={photo.owner}
+          license={photo.license}
+        />
+      </div>
+    </ScrollBox>
+  ) : (
+    <div />
+  );
+}
